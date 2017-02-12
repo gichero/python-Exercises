0274a344bde146bfd7351890c25330db34dfa77d
Amos <amosgathungu@gmail.com>
2017-02-11 21:14:34 -0500

    functions

---
+++ drawSquare.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+def drawSquare():
+    for i in range(4):
+
+        forward(100)
+        left(90)
+if __name__ == '__main__':
+    drawSquare()
+
+up()
+forward(200)
+down()
+
+drawSquare()
+
+mainloop()
---
+++ functionsPython/functionSquare.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plot
+
+def f(x):
+    return x * x
+xs = range(-100, 100)
+ys = []
+
+for x in xs:
+    ys.append(f(x))
+plot.plot(xs, ys)
+plot.show()
